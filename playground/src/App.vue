af78740fcc92d40a19ee1405d89506a37e03d001
David Oviedo <moraleja39@gmail.com>
2025-11-12 15:07:54 +0100

    more demo styles

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -5,20 +5,31 @@ import InputText from 'primevue/inputtext'
 import Select from 'primevue/select'
 import { ref } from 'vue'
 
-const color = ref('oklch(43% 0.148 343.0)')
-const color2 = ref('oklch(43% 0.1 127)')
+const color = ref('oklch(0.430 0.148 343.0)')
+const color2 = ref('oklch(0.5 0.2 35)')
+// const color3 = ref('oklch(0.632 0.185 275.0)')
 </script>
 
 <template>
-  <main>
-    <h1>Vue colorjs.io picker</h1>
-    <div class="">
-      <h3>Default</h3>
-      <ColorPicker v-model="color" class="w-120 rounded-sm border" />
+  <main class="min-h-lvh grid grid-cols-[max-content_1fr] grid-rows-[max-content_max-content_1fr] gap-18 p-6">
+    <h1 class="text-3xl font-semibold col-span-2">
+      Vue <a href="https://colorjs.io" target="_blank">colorjs.io</a> picker
+    </h1>
+    <div>
+      <h3 class="text-xl mb-3">
+        Default
+      </h3>
+      <ColorPicker v-model="color" class="w-120 rounded-sm border border-border" />
+      <div class="mt-2">
+        Picked color: {{ color }}
+      </div>
     </div>
+    <div class="example" />
     <div class="">
-      <h3>With custom controls</h3>
-      <ColorPicker v-model="color2" class="w-120 rounded-sm border">
+      <h3 class="text-xl mb-3">
+        With custom controls
+      </h3>
+      <ColorPicker v-model="color2" class="w-120 rounded-sm border border-border">
         <template #space-select="{ options, value, onUpdate }">
           <Select class="w-full" :options="options.slice()" :model-value="value" @update:model-value="onUpdate" />
         </template>
@@ -31,7 +42,9 @@ const color2 = ref('oklch(43% 0.1 127)')
           <InputText class="text-xs!" size="small" fluid :model-value="value" @update:model-value="onUpdate($event ?? '')" />
         </template>
       </ColorPicker>
+      <div class="mt-2">
+        Picked color: {{ color2 }}
+      </div>
     </div>
   </main>
 </template>
-
---
+++ playground/src/components/Separator.vue
@@ -0,0 +1,3 @@
+<template>
+  <hr class="h-0 my-4 border-b border-b-border border-dashed">
+</template>
