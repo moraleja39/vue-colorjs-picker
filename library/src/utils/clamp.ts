6ff2896541384673d18453c41275c457308d0284
David Oviedo <moraleja39@gmail.com>
2025-11-13 11:13:24 +0100

    fixed the clamp implementation 😅

--- library/src/utils/clamp.ts
+++ library/src/utils/clamp.ts
@@ -1,3 +1,3 @@
-export function clamp(value: number, min: number, max: number) {
-  return Math.max(value, Math.min(value, max))
+export function clamp(value: number, min: number, max: number): number {
+  return Math.max(min, Math.min(value, max))
 }
